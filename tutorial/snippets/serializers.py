c641e28911a471b5ce48629c3392f5dd1d917166
bFraley <bFraley@users.noreply.github.com>
2016-10-11 09:40:35 -0400

    section 5 complete, adds Hyperlinking between entities and pagination

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -7,23 +7,22 @@ It's important to remember that ModelSerializer classes don't do
 anything particularly magical, they are simply a shortcut for creating
 serializer classes:
 """
-
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'pk', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
 
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
-
+        fields = ('url', 'pk', 'username', 'snippets')
 
 
 
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -115,6 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
